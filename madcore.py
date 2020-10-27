44a4be5c9cded8f71a575c02fa861a489d2e93bf
kirasys <kirasys0@naver.com>
2020-10-27 17:56:36 +0900

    Add a option for saving interface

--- madcore.py
+++ madcore.py
@@ -1,5 +1,6 @@
 import sys
 import angr
+import json
 import pprint
 import logging
 import datetime
@@ -9,15 +10,16 @@ import boltons.timeutils
 from projects import wdm
         
 def setup_logging(args):
-    level = getattr(logging, args.loglvl)
+    level = getattr(logging, args.loglevel)
     logging.getLogger('angr').setLevel(level)
 
 if __name__ == '__main__':
     start_time = datetime.datetime.utcnow()
-    parser = argparse.ArgumentParser(description='Automatic Driver Analysis', usage='driver.py [-d, --driver] [driverPath] [-L, --log] [logLevel] [-s, --skip]')
-    parser.add_argument('-d', '--driver', dest='driver', help='driverPath')
-    parser.add_argument('-L', '--log', default='FATAL', dest='loglvl', choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'FATAL'), help='set the logging level')
+    parser = argparse.ArgumentParser(description='Automatic Driver Analysis', usage='driver.py [-d, --driver] driverPath [-L, --log] logLevel [-s, --skip] [-o, --output] output')
+    parser.add_argument('-d', '--driver', dest='driver', help='path to the driver')
+    parser.add_argument('-L', '--log', default='FATAL', dest='loglevel', choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'FATAL'), help='set the logging level')
     parser.add_argument('-s', '--skip', dest='skip', action='store_true', help='skip the functions that do not need to be analyzed')
+    parser.add_argument('-o', '--output', dest='output', help='path to the output')
     args = parser.parse_args()
     setup_logging(args)
 
@@ -41,6 +43,10 @@ if __name__ == '__main__':
 
         elapsed = boltons.timeutils.decimal_relative_time(start_time, datetime.datetime.utcnow())
         print("[*] completed in: {0:.1f} {1}".format(*elapsed))
+
+        if args.output:
+            with open(args.output, "w") as json_file:
+                json.dump(ioctl_interface, json_file)
     else:
         print("[!] '%s' is not a supported driver." % args.driver)
         sys.exit()
